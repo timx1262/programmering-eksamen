b513ac624f9e71a06d168368baa8c7d5f9a7d812
Tim Hounsgaard <34690331+timx1262@users.noreply.github.com>
2020-03-02 15:32:46 +0100

    shit it do not work

--- gameManager.js
+++ gameManager.js
@@ -9,4 +9,6 @@ function draw(){
     player();
 
     playerMovement();
+
+    drawSprites();
 }
--- player.js
+++ player.js
@@ -1,21 +1,26 @@
 var playerspeed = 1;
+var ply1;
+var ply2;
 var player1X = 1536*0.3333333333;
 var player1Y = 753/2;
 var player2X = 1536*0.6666666666;
 var player2Y = 753/2;
 function player(){
 
-    rect(player1X, player1Y, 32, 32);
-    rect(player2X, player2Y, 32, 32);
+    ply1 = createSprite(100, 100, 64, 64);
+    ply1.shapeColor = color(255, 0, 0);
+
+    ply2 = createSprite(player2X, player2Y, 64, 64);
+    ply2.shapeColor = color(0, 0, 255)
+
 
 }
 
 
  function playerMovement(){
 
-    if(keyPressed){
-        key == "UP"
-        player1X = player1X + playerspeed;
+    if(keyDown(87)){
+        ply1.postion.y = ply1.postion.y - playerspeed;
     }
 
 
